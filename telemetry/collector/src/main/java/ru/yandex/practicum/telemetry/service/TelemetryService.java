d64920cf113dfbbd8290a54e77b5ad4c7bfe9c72
naviwe <timurnovotorzhin@yandex.kz>
2025-05-08 21:18:55 +0400

    fix: fixed

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/service/TelemetryService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/service/TelemetryService.java
@@ -1,51 +1,80 @@
 package ru.yandex.practicum.telemetry.service;
 
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.telemetry.config.KafkaConfig;
 import ru.yandex.practicum.telemetry.mapper.HubEventAvroMapper;
 import ru.yandex.practicum.telemetry.mapper.SensorEventAvroMapper;
 import ru.yandex.practicum.telemetry.model.HubEvent;
 import ru.yandex.practicum.telemetry.model.SensorEvent;
 
-import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
-
-import static ru.yandex.practicum.telemetry.config.KafkaTopicsConfig.HUB_EVENT_TOPIC;
-import static ru.yandex.practicum.telemetry.config.KafkaTopicsConfig.SENSOR_EVENT_TOPIC;
+import java.util.Properties;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class TelemetryService {
-    private final KafkaTemplate<String, HubEventAvro> kafkaHubTemplate;
-    private final KafkaTemplate<String, SensorEventAvro> kafkaSensorTemplate;
+
+    private final KafkaProducer<String, SpecificRecordBase> producer;
+    private final KafkaConfig kafkaConfig;
+
+    public TelemetryService(KafkaConfig kafkaConfig) {
+        this.kafkaConfig = kafkaConfig;
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getBootstrapServers());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaConfig.getProducer().getKeySerializer());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaConfig.getProducer().getValueSerializer());
+
+        this.producer = new KafkaProducer<>(props);
+    }
 
     public void processSensorEvent(SensorEvent event) {
-        SensorEventAvro sensorEventAvro = SensorEventAvroMapper.mapSensorEvent(event);
-        CompletableFuture<SendResult<String, SensorEventAvro>> future =
-                kafkaSensorTemplate.send(SENSOR_EVENT_TOPIC.getTopic(), sensorEventAvro.getHubId(), sensorEventAvro);
-
-        future.whenComplete((result, ex) -> {
-            if (Objects.nonNull(ex)) {
-                log.error(ex.getMessage(), ex);
-            }
-        });
+        try {
+            SensorEventAvro sensorEventAvro = SensorEventAvroMapper.mapSensorEvent(event);
+            String topic = kafkaConfig.getTopics().getSensor();
+            ProducerRecord<String, SpecificRecordBase> record =
+                    new ProducerRecord<>(topic, sensorEventAvro.getHubId(), sensorEventAvro);
+            producer.send(record, (metadata, exception) -> {
+                if (exception != null) {
+                    log.error("Error sending sensor event to topic [{}]: {}", topic, exception.getMessage(), exception);
+                } else {
+                    log.info("Sensor event sent to topic [{}] with offset {}", topic, metadata.offset());
+                }
+            });
+        } catch (Exception ex) {
+            log.error("Error processing sensor event: {}", ex.getMessage(), ex);
+        }
     }
 
     public void processHubEvent(HubEvent event) {
-        HubEventAvro hubEventAvro = HubEventAvroMapper.mapSensorEvent(event);
-        CompletableFuture<SendResult<String, HubEventAvro>> future =
-                kafkaHubTemplate.send(HUB_EVENT_TOPIC.getTopic(), event.getType().name(), hubEventAvro);
+        try {
+            HubEventAvro hubEventAvro = HubEventAvroMapper.mapSensorEvent(event);
+            String topic = kafkaConfig.getTopics().getHub();
+            ProducerRecord<String, SpecificRecordBase> record =
+                    new ProducerRecord<>(topic, event.getType().name(), hubEventAvro);
+            producer.send(record, (metadata, exception) -> {
+                if (exception != null) {
+                    log.error("Error sending hub event to topic [{}]: {}", topic, exception.getMessage(), exception);
+                } else {
+                    log.info("Hub event sent to topic [{}] with offset {}", topic, metadata.offset());
+                }
+            });
+        } catch (Exception ex) {
+            log.error("Error processing hub event: {}", ex.getMessage(), ex);
+        }
+    }
 
-        future.whenComplete((result, ex) -> {
-            if (Objects.nonNull(ex)) {
-                log.error(ex.getMessage(), ex);
-            }
-        });
+    @PreDestroy
+    public void close() {
+        producer.close();
     }
 }
