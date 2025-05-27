e825a5a1d393ca15e797c6414b8d8b901e3f064c
naviwe <timurnovotorzhin@yandex.kz>
2025-05-28 00:51:22 +0400

    fix: collector

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/kafka/EventProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/kafka/EventProducer.java
@@ -4,8 +4,8 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Properties;
@@ -16,16 +16,15 @@ public class EventProducer {
 
     private final KafkaProducer<String, SpecificRecordBase> producer;
 
-    public EventProducer() {
-        this.producer = new KafkaProducer<>(producerProperties());
-    }
-
-    private Properties producerProperties() {
+    public EventProducer(
+            @Value("${spring.kafka.producer.bootstrap-servers}") String bootstrapServers,
+            @Value("${spring.kafka.producer.key-serializer}") String keySerializer,
+            @Value("${spring.kafka.producer.value-serializer}") String valueSerializer) {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class.getName());
-        return props;
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        this.producer = new KafkaProducer<>(props);
     }
 
     public Future<RecordMetadata> send(String topic, String key, SpecificRecordBase value) {
