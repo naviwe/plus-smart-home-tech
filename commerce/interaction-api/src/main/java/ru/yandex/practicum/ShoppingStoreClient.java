5e3ff19cc34ca9ed256f65b967f52c4e6438081a
naviwe <timurnovotorzhin@yandex.kz>
2025-06-17 21:57:51 +0400

    fix: shopping-store

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/ShoppingStoreClient.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.dto.shoppingstore.Pageable;
+import ru.yandex.practicum.dto.shoppingstore.ProductDto;
+import ru.yandex.practicum.enums.ProductCategory;
+import ru.yandex.practicum.enums.QuantityState;
+
+@FeignClient(name = "shopping-store-service", url = "${shopping-store.service.url}", path = "/api/v1/shopping-store")
+public interface ShoppingStoreClient {
+
+    @GetMapping
+    Page<ProductDto> getProductsByCategory(
+            @RequestParam ProductCategory category,
+            @RequestBody Pageable pageable);
+
+    @PutMapping
+    ProductDto createProduct(@RequestBody ProductDto productDto);
+
+    @PostMapping
+    ProductDto updateProduct(@RequestBody ProductDto productDto);
+
+    @PostMapping("/removeProductFromStore")
+    boolean removeProduct(@RequestBody String productId);
+
+    @PostMapping("/quantityState")
+    boolean changeState(
+            @RequestParam String productId,
+            @RequestParam QuantityState quantityState);
+
+    @GetMapping("/{productId}")
+    ProductDto getInfoByProduct(@PathVariable String productId);
+}
